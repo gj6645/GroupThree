a4e177a0ed254dc4cbd0be895fb5914836c67f7a
Viona Veseli <gj6645@wayne.edu>
2021-10-14 14:11:10 -0400

    Routing added, main and authors page added with no data inside them

---
+++ client/src/components/Main.js
@@ -0,0 +1,14 @@
+
+import React from "react"
+
+export default function Main() {
+
+    return (
+        <div className="Main">
+
+            <p> Main</p>
+
+            <a href="/authors">Click here for authors</a>
+        </div>
+    )
+}
