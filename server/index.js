9c4ebb9da50c4a37e597628deb2f6f2258f7c94d
CalebObsCode <Sommskycal345@gmail.com>
2021-11-06 10:52:13 -0400

    Fixing DB timeout issue

--- server/index.js
+++ server/index.js
@@ -32,13 +32,14 @@ app.use((err, req, res, next) => {
 });
 
 
-const db = mysql.createConnection({
+
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
-    
 });
+
 
 
 
