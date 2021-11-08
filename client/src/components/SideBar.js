91b6c5359c8968793f7183b7aefd8a78222f0415
Viona Veseli <gj6645@wayne.edu>
2021-11-08 04:14:23 -0500

    route and renaming

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -55,11 +55,11 @@ export default function SideBar() {
                 </List>
                 <List>
 
-                    <ListItemButton button component={Link} href="/authors">
+                    <ListItemButton button component={Link} href="/alltasks">
                         <ListItemText primary="All Tasks" />
                     </ListItemButton>
 
-                    <ListItem button component={Link} href="/authors">
+                    <ListItem button component={Link} href="/category">
                         <ListItemText primary="Category" />
                     </ListItem>
 
@@ -71,14 +71,14 @@ export default function SideBar() {
                     </ListItemButton>
                     <Collapse in={open} timeout="auto" unmountOnExit>
                         <List component="div" disablePadding>
-                            <ListItemButton sx={{ pl: 4 }}>
-                                <ListItemText primary="Link1" />
+                            <ListItemButton button component={Link} href="/categoryoptions" sx={{ pl: 4 }}>
+                                <ListItemText primary="Category Options" />
                             </ListItemButton>
-                            <ListItemButton sx={{ pl: 4 }}>
-                                <ListItemText primary="Link2" />
+                            <ListItemButton button component={Link} href="/completedtasks" sx={{ pl: 4 }}>
+                                <ListItemText primary="Completed Tasks" />
                             </ListItemButton>
-                            <ListItemButton sx={{ pl: 4 }}>
-                                <ListItemText primary="Link3" />
+                            <ListItemButton button component={Link} href="/priorityoptions" sx={{ pl: 4 }}>
+                                <ListItemText primary="Priority Options" />
                             </ListItemButton>
                         </List>
                     </Collapse>
