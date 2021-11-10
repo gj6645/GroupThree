2073d92bb2c32707d042f7244ab105f2f5e17b73
Viona Veseli <gj6645@wayne.edu>
2021-11-09 23:48:59 -0500

    fixes to packages
    tables now working, npm installs in right place. packages updated.

--- client/src/useEffect/index.js
+++ client/src/useEffect/index.js
@@ -1,20 +1,20 @@
-// import React, { useState, useEffect } from "react"
-// //tables
-// import Datatable from '../datatable/index';
+import React, { useState, useEffect } from "react"
+//tables
+import Datatable from '../datatable/index';
 
-// export default function UseEffect({ link }) {
-//     //requirements for fetch 
-//     require('es6-promise').polyfill();
-//     require('isomorphic-fetch');
+export default function UseEffect({ link }) {
+    //requirements for fetch 
+    require('es6-promise').polyfill();
+    require('isomorphic-fetch');
     
-//     //function for table
-//     const [data,setData] = useState([])
-//     useEffect(() => {
-//         fetch('https://csc4710dbs.herokuapp.com/api/getTasks')
-//           .then((response) => response.json())
-//           .then((json) => setData(json));
-//       }, []);
-//         return(
-//             <Datatable data={(data)} />
-//         )
-// }
+    //function for table
+    const [data,setData] = useState([])
+    useEffect(() => {
+        fetch('https://csc4710dbs.herokuapp.com/api/getTasks')
+          .then((response) => response.json())
+          .then((json) => setData(json));
+      }, []);
+        return(
+            <Datatable data={(data)} />
+        )
+}
