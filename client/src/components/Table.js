7d17cb344034f70096ee2ebf553d69b42c34598a
CalebObsCode <Sommskycal345@gmail.com>
2021-11-09 21:44:48 -0500

    Testing material ui table

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,6 +1,31 @@
 import React from 'react'
+import MaterialTable from 'material-table';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
 
 export default function Table() {
+
+    const [Data, setData] = useState([]);
+
+    let columns = [
+        { title: 'DESCRIPTION', field: 'description' },
+        { title: 'PRIORITY', field: 'priority' },
+        { title: 'CATEGORIES', field: 'categories' },
+        { title: 'STATUS', field: 'status' },
+        { title: 'DUEDATE', field: 'duedate' },
+      ]
+
+      useEffect(() => {
+        axios.get('https://csc4710dbs.herokuapp.com/api/getTasks')
+        .then((response) => response.json())
+        .then((json) => setData(json));
+        }, []);
+        
+
+
+
     return (
         <div>
             
