a49d3727804ee896f918a5096673e77e742272ab
eishabar <82237730+eishabar@users.noreply.github.com>
2021-11-12 12:25:29 -0500

    Modifying Table Size

--- client/src/components/tables/AllTasksTable.js
+++ client/src/components/tables/AllTasksTable.js
@@ -74,7 +74,7 @@ export default function StickyHeadTable() {
         
     // return setRows data to paper sx
     <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-        <TableContainer sx={{ maxHeight: 440 }}>
+        <TableContainer sx={{ maxHeight: 660 }}>
             <Table stickyHeader aria-label="sticky table">
                 <TableHead>
                     <TableRow>
