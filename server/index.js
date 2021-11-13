b33d55541b54616bea86948b55e27b901a4aa738
CalebObsCode <Sommskycal345@gmail.com>
2021-11-13 08:25:48 -0500

    Adding delete functionality in category page

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql2')
 const cors = require('cors');
+const responseEnhancer = require('express-response-formatter');
 
 
 const PORT = 3001;
@@ -32,30 +33,12 @@ app.use((err, req, res, next) => {
 });
 
 
-
-
 const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
-
-
-// set interval to never expire
-setInterval(function () {
-    db.query('SELECT * FROM tasks');
-}, 30000);
-
-// Using set interval to check if the connection is still alive and if not, it will reconnect
-setInterval(function () {
-    db.query('SELECT * FROM tasks');
-}, 30000);
-
-
-
-
-
 
 
 
@@ -79,7 +62,7 @@ app.get('/api/createDB', (req, res) => {
 
 // create task table in database
 app.get('/api/createTasksTable', (req, res) => {
-    let sql = "CREATE TABLE tasks(Tasks_id int AUTO_INCREMENT, tasks_name VARCHAR(255), tasks_description VARCHAR(255), tasks_priority VARCHAR(255), Categories_id int, tasks_categories VARCHAR(10), tasks_status VARCHAR(255), tasks_due_date datetime, PRIMARY KEY(Tasks_id), FOREIGN KEY(Categories_id) REFERENCES categories(Categories_id))";
+    let sql = "CREATE TABLE tasks(Tasks_id int AUTO_INCREMENT, tasks_description VARCHAR(255), tasks_priority VARCHAR(255), Categories_id int, tasks_categories VARCHAR(10), tasks_status VARCHAR(255), tasks_due_date datetime, PRIMARY KEY(Tasks_id), FOREIGN KEY(Categories_id) REFERENCES categories(Categories_id))";
     db.query(sql, (err, result) => {
         if (err) throw err;
         console.log(result);
@@ -112,6 +95,7 @@ app.post('/api/createTask', (req, res) => {
     const Categories_id = req.body.Categories_id;
     const tasks_priority = req.body.tasks_priority;
     const tasks_due_date = req.body.tasks_due_date;
+    tasks_due_date
     const tasks_status = req.body.tasks_status;
     db.query('INSERT INTO tasks (tasks_description, tasks_categories, Categories_id, tasks_priority, tasks_status, tasks_due_date) VALUES (?, ?, ?, ?, ?, ?)',
     [tasks_description, tasks_categories, Categories_id, tasks_priority, tasks_status, tasks_due_date], (err, result) => {
@@ -122,12 +106,41 @@ app.post('/api/createTask', (req, res) => {
 });
 
 
+// POST API to insert categories into database
+app.post('/api/createCategory', (req, res) => {
+    const tasks_categories = req.body.tasks_categories;
+    db.query('INSERT INTO categories (tasks_categories) VALUES (?)', [tasks_categories], (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Category inserted...');
+    });
+});
+
+
+
+
 /*
  *******************************************
  ********* GET CRUD API HERE **************
  *******************************************
 */
-// GET API to get all tasks from database
+app.get('/api/getTasksFormatted', (req, res) => {
+    db.query('SELECT * FROM tasks', (err, result) => {
+        if (err) throw err;
+        console.log(result);
+
+        let apiResponse = {
+            result: result
+        };
+
+        res.send(apiResponse);
+
+    });
+});
+
+
+
+
 app.get('/api/getTasks', (req, res) => {
     db.query('SELECT * FROM tasks',
     (err, rows, fields) => {
@@ -136,14 +149,11 @@ app.get('/api/getTasks', (req, res) => {
             res.header("Content-Type",'application/json');
             //res.send(JSON.stringify(rows));
             res.type('json').send(JSON.stringify(rows, null, 2) + '\n');
-            //res.send(rows);
         } else {
             console.log(err);
         }
     });
 });
-
-
 
 
 
@@ -167,9 +177,9 @@ app.get('/api/getTasksToday', (req, res) => {
     db.query('SELECT * FROM tasks WHERE tasks_due_date = curdate()',
     (err, rows, fields) => {
         if (!err) {
-            //res.send(rows);
+            
             res.header("Content-Type",'application/json');
-            //res.send(JSON.stringify(rows));
+            
             res.type('json').send(JSON.stringify(rows, null, 2) + '\n');
         
         } else {
@@ -180,12 +190,12 @@ app.get('/api/getTasksToday', (req, res) => {
 
 // GET API to get overdue tasks
 app.get('/api/getOverdueTasks', (req, res) => {
-    db.query('SELECT * FROM tasks WHERE tasks_due_date < curdate()',
+    db.query('SELECT * FROM tasks WHERE tasks_due_date < curdate() AND tasks_status = "Active"',
     (err, rows, fields) => {
         if (!err) {
-            //res.send(rows);
+            
             res.header("Content-Type",'application/json');
-            //res.send(JSON.stringify(rows));
+            
             res.type('json').send(JSON.stringify(rows, null, 2) + '\n');
         
         } else {
@@ -195,13 +205,19 @@ app.get('/api/getOverdueTasks', (req, res) => {
 });
 
 
-// GET API to get tasks based on dropdown category selection
-app.get('/api/getTasks/:category', (req, res) => {
-    const category = req.params.category;
-    db.query('SELECT * FROM tasks WHERE tasks_categories = ?', [category], (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send(result);
+
+// GET API to get all categories and render as a dropdown in a form
+app.get('/api/getCategories', (req, res) => {
+    db.query('SELECT * FROM categories', (err, rows, fields) => {
+        if (!err) {
+
+            res.header("Content-Type",'application/json');
+
+            res.type('json').send(JSON.stringify(rows, null, 2) + '\n');
+
+        } else {
+            console.log(err);
+        }
     });
 });
 
@@ -231,6 +247,22 @@ app.put('/api/updateTask/:Tasks_id', (req, res) => {
     );
 });
 
+// PUT API to update a category in database
+app.put('/api/updateCategory/:Categories_id', (req, res) => {
+    const Categories_id = req.params.Categories_id;
+    const tasks_categories = req.body.tasks_categories;
+    db.query('UPDATE categories SET tasks_categories = ? WHERE Categories_id = ?',
+    [tasks_categories, Categories_id],
+    (err, result) => {
+
+        if (err) throw err;
+        console.log(result);
+        res.send('Category updated...');
+    }
+    );
+});
+
+
 
 /*
  *******************************************
@@ -250,10 +282,29 @@ app.delete('/api/deleteTask/:Tasks_id', (req, res) => {
         }
     });
 });
+
+
+// DELETE API to delete a category from database
+app.delete('/api/deleteCategory/:Categories_id', (req, res) => {
+    const Categories_id = req.params.Categories_id;
+    db.query('DELETE FROM categories WHERE Categories_id = ?',
+    [Categories_id],
+    (err, rows, fields) => {
+        if (!err) {
+            res.send(rows);
+
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+
+
+
 app.listen(process.env.PORT || PORT, () => {
     console.log('Server is running on port 3001');
 });
-
 
 
 module.exports = db.promise();
@@ -267,9 +318,6 @@ module.exports = db.promise();
     - Display Task based on Priority (e.g. Priority 1, Priority 2, Priority 3, Priority 4)
     - Display Task based on Status (e.g. Completed, Active)
 */
-
-// Auto reset auto increment value to 1
-// ALTER TABLE tasks AUTO_INCREMENT = 1;
 
 
 // API ENDPOINT that Needs to be developed
