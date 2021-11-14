c0103b10320f69a98a1800835ddeda3d4e137878
CalebObsCode <Sommskycal345@gmail.com>
2021-11-13 19:16:02 -0500

    Added Button on category page

--- client/src/pages/Category.js
+++ client/src/pages/Category.js
@@ -5,9 +5,17 @@ import SideBar from "../components/SideBar"
 import { Box } from "@mui/system"
 import Toolbar from "@mui/material/Toolbar";
 import CategoryTables from '../components/tables/CategoryTables';
+import Button from '@mui/material/Button';
+
+
+
 export default function Category() {
 
     return (
+
+        <>
+        <div className="text-center">
+
         <Box sx={{ display: 'flex' }}>
             <Header />
             <SideBar />
@@ -17,10 +25,28 @@ export default function Category() {
             >
 
                 <Toolbar />
+                <br></br>
+                                
+
+                        {/* Button for add new task */}
+                        <Button variant="contained" >
+                            <span class="material-icons" style={{padding: "1px"}} >add</span>
+                            Add New Category
+                        </Button>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                
                 <h3>Categories</h3>
+
+                <br></br>
+
                 <hr></hr>
                 <CategoryTables />
             </Box>
         </Box >
+        </div>
+
+        </>
     )
 }
