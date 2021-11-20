d3b2525b07cc2843e1fa1de2b4d4b86d5cfcb0b8
CalebObsCode <Sommskycal345@gmail.com>
2021-11-19 21:25:45 -0500

    Filter API call sample

--- client/src/components/tables/PriorityTable.js
+++ client/src/components/tables/PriorityTable.js
@@ -11,6 +11,7 @@ import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Button from '@mui/material/Button';
 import axios, { Axios } from "axios";
+import { MenuItem, FormControl, Select, InputLabel } from "@mui/material";
 
 
 
@@ -33,14 +34,17 @@ export default function StickyHeadTable() {
 
     const [rows, setRows] = useState([]);
 
-    // As of right now, this causes the page to crash
-    /*useEffect(() => {
-        fetch('http://localhost:3001/api/getTasksByPriority/:priority')
-            .then((response) => response.json())
-            .then((json) => setRows(json));
+    // useEffect to display all tasks
+    useEffect(() => {
+        axios.get("https://csc4710dbs.herokuapp.com/api/getTasks")
+            .then(res => {
+                setRows(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
 
-
-    }, []);*/
+    }, []);
 
 
     const [page, setPage] = React.useState(0);
@@ -73,7 +77,51 @@ export default function StickyHeadTable() {
     }
 
 
+    // If priority 1 is picked, call API https://csc4710dbs.herokuapp.com/api/getTasksByPriority/:tasks_priority and pass in priority 1 and display the tasks for that priority
+    // If priority 2 is picked, call API https://csc4710dbs.herokuapp.com/api/getTasksByPriority/:tasks_priority and pass in priority 2 and display the tasks for that priority
+    // If priority 3 is picked, call API https://csc4710dbs.herokuapp.com/api/getTasksByPriority/:tasks_priority and pass in priority 3 and display the tasks for that priority
+    // If priority 4 is picked, call API https://csc4710dbs.herokuapp.com/api/getTasksByPriority/:tasks_priority and pass in priority 4 and display the tasks for that priority
     
+    const prioritySelection = (event) => {
+        if (event.target.value === "Priority 1") {
+            axios.get("https://csc4710dbs.herokuapp.com/api/getTasksByPriority/Priority 1")
+                .then(res => {
+                    setRows(res.data);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+        else if (event.target.value === "Priority 2") {
+            axios.get("https://csc4710dbs.herokuapp.com/api/getTasksByPriority/Priority 2")
+                .then(res => {
+                    setRows(res.data);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+        else if (event.target.value === "Priority 3") {
+            axios.get("https://csc4710dbs.herokuapp.com/api/getTasksByPriority/Priority 3")
+
+                .then(res => {
+                    setRows(res.data);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+        else if (event.target.value === "Priority 4") {
+            axios.get("https://csc4710dbs.herokuapp.com/api/getTasksByPriority/Priority 4")
+                .then(res => {
+                    setRows(res.data);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+
 
 
 
@@ -81,104 +129,83 @@ export default function StickyHeadTable() {
 
         // return setRows data to paper sx
         <>
-
-            <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-                <TableContainer sx={{ maxHeight: 440 }}>
-                    <Table stickyHeader aria-label="sticky table">
-                        <TableHead>
-                            <TableRow>
-                                {columns.map((column) => (
-                                    <TableCell
-                                        key={column.id}
-                                        align={column.align}
-                                        style={{ minWidth: column.minWidth }}
-
-                                        actionsColumnIndex={-1}
-                                    >
-                                        {column.label}
-
+        {/* Create a priority dropdown and get value for each value and display table based on what was picked */}
+        <Paper>
+            <TableContainer>
+                <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                            {columns.map((column) => (
+                                <TableCell
+                                    key={column.id}
+                                    align={column.align}
+                                    style={{ minWidth: column.minWidth }}
+                                >
+                                    {column.label}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                            return (
+                                <TableRow hover role="checkbox" tabIndex={-1} key={row.Tasks_id}>
+                                    {columns.map((column) => {
+                                        const value = row[column.id];
+                                        return (
+                                            <TableCell key={column.id} align={column.align}>
+                                                {column.format && typeof value === 'number' ? column.format(value) : value}
+                                            </TableCell>
+                                        );
+                                    })}
+                                    <TableCell align="right">
+                                        <Button variant="contained" color="primary" onClick={() => deleteTask(row.Tasks_id)}>Delete</Button>
                                     </TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
+        </Paper>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
 
-                                ))}
-
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {/* 
-                        Rows: {
-                            Tasks_id,
-                            Tasks_description,
-                            Tasks_due_date,
-                            Tasks_priority,
-                            Tasks_category,
-                            Tasks_status
-
-                        }
-                    */}
-                            {/* For each row, add a edit and delete material ui button */}
-                            {/* Add <EditIcon />*/}
-
-                            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                                return (
-                                    <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-
-                                        {columns.map((column) => {
-                                            const value = row[column.id];
-                                            return (
-                                                <TableCell key={column.id} align={column.align}>
-                                                    {column.format && typeof value === 'number' ? column.format(value) : value}
-                                                </TableCell>
-
-                                            );
-
-
-                                        })}
-
-
-                                        {/* Edit icon on each row*/}
-                                        <TableCell
-                                            style={{ minWidth: 17, align: 'right', color: '#1972d8', size: 'x-small' }}
-                                        >{row.Tasks_actions}
-                                            <EditIcon />
-                                        </TableCell>
-
-
-                                        {/* Delete icon on each row*/}
-                                        <TableCell
-                                            style={{ minWidth: 17, align: 'right', color: '#1972d8', size: 'x-small' }}
-                                        >{row.Tasks_actions}
-                                            <Button onClick={() => {
-                                                deleteTask(row.Tasks_id);
-                                            }}>
-                                                <DeleteIcon
-
-                                                />
-                                            </Button>
-                                        </TableCell>
-
-                                    </TableRow>
-
-                                );
-                            })}
-
-
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <TablePagination
-                    rowsPerPageOptions={[10, 25, 100]}
-                    component="div"
-                    count={rows.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-
-                />
-            </Paper>
-
-
+        <div>
+            <FormControl
+            sx={{
+                width: "100"
+            }}
+            >
+                <InputLabel id="demo-simple-select-label">Priority</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={prioritySelection}
+                    onChange={prioritySelection}
+                    sx={{
+                        width: "100"}}
+                    
+                >
+                    <MenuItem value={"Priority 1"}>Priority 1</MenuItem>
+                    <MenuItem value={"Priority 2"}>Priority 2</MenuItem>
+                    <MenuItem value={"Priority 3"}>Priority 3</MenuItem>
+                    <MenuItem value={"Priority 4"}>Priority 4</MenuItem>
+                </Select>
+            </FormControl>
+        </div>
         </>
-
     );
 }
+
