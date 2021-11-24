18e6e4007e3f7bda35a2b0e4332d701ee82b7076
tanvirahmed725 <91294146+tanvirahmed725@users.noreply.github.com>
2021-11-24 01:20:22 -0500

    Updated Completed tasks table

--- client/src/pages/CompletedTasks.js
+++ client/src/pages/CompletedTasks.js
@@ -3,7 +3,7 @@ import Header from "../components/Header"
 import SideBar from "../components/SideBar"
 import { Box } from "@mui/system"
 import Toolbar from "@mui/material/Toolbar";
-
+import CompletedTasksTable from "../components/tables/CompletedTasksTable"; 
 export default function CompletedTasks() {
 
 
@@ -21,6 +21,7 @@ export default function CompletedTasks() {
                 <Toolbar />
                 <h3>Completed Tasks</h3>
                 <hr></hr>
+                <CompletedTasksTable/>
             </Box>
         </Box >
 
